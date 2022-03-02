b681204b9ecce8d81149afe390a0dfc9e52ade03
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-03-02 10:37:43 -0500

    New project. Added required dependencies

---
+++ src/main/java/com/appsdeveloperblog/photoapp/oauthserver/NewSpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/photoapp/oauthserver/NewSpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
