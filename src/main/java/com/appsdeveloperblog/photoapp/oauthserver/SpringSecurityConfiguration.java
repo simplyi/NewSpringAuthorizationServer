958bdfce255aafd45ad3e000f27763544ed557cd
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-03-03 14:48:15 -0500

    Spring Security Configuration

---
+++ src/main/java/com/appsdeveloperblog/photoapp/oauthserver/SpringSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.appsdeveloperblog.photoapp.oauthserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SpringSecurityConfiguration {
+
+	@Bean
+	SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+		.formLogin(Customizer.withDefaults());
+		
+		return http.build();
+		
+	}
+	
+
+	@Bean
+	public UserDetailsService users() {
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		UserDetails user = User.withUsername("sergey")
+				.password(encoder.encode("password"))
+				.roles("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user);
+		
+	}
+	
+}
